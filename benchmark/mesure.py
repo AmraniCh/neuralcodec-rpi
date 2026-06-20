30b68c87b606a5a09d3dee93ec5d3db22ae1470e
AmraniCh <elamrani.sv.laza@gmail.com>
2026-06-20 23:29:27 +0100

    feat (benchmark): add compression percentage calculation function

--- benchmark/mesure.py
+++ benchmark/mesure.py
@@ -4,6 +4,8 @@ from audio_io import get_audio_info
 from audio_io import load_audio
 from pesq import pesq
 
+def get_file_size(path):
+    return os.path.getsize(path)
 
 def calc_bitrate(path):
     # for a compressed audio we can use the formula 
@@ -18,13 +20,15 @@ def calc_pesq(original_path, decoded_path):
     decoded_vector = load_audio(decoded_path)[0]
     return pesq(fs=16000, ref=original_vector, deg=decoded_vector, mode='wb')
 
-def get_file_size(path):
-    return os.path.getsize(path)
-
+def calc_compression_percentage(original_path, compressed_path):
+    og_size_bytes = get_file_size(original_path)
+    cp_size_bytes = get_file_size(compressed_path)
+    return 100 - ((cp_size_bytes* 100) / og_size_bytes)
 
 if __name__ == "__main__":
     import sys
 
+    # compressed sound file
     if len(sys.argv) == 4:
         original_path = sys.argv[1]
         decoded_path = sys.argv[2]
@@ -33,7 +37,9 @@ if __name__ == "__main__":
         print("Bitrate (kbps) - Original: ", calc_bitrate(original_path))
         print("Bitrate (kbps) - Decoded: ", calc_bitrate(decoded_path))
         print("Bitrate (kbps) - Compressed: ", calc_bitrate(compressed_path))
+        print("Compression %: ", calc_compression_percentage(original_path, compressed_path))
 
+    # whatever sound file
     elif len(sys.argv) == 2:
         sound_file = sys.argv[1]
         print("Filesize (bytes):", get_file_size(sound_file))
