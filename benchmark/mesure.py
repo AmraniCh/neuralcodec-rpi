b438bfce345970f7f858c1dbbf27e216da61a9ea
AmraniCh <elamrani.sv.laza@gmail.com>
2026-06-20 02:22:30 +0100

    feat(mesure): add PESQ calculation function and update bitrate logic

--- benchmark/mesure.py
+++ benchmark/mesure.py
@@ -1,14 +1,22 @@
 import os
 from pprint import pprint
 from audio_io import get_audio_info
+from audio_io import load_audio
+from pesq import pesq
+
 
 def calc_bitrate(path):
     # for a compressed audio we can use the formula 
     # bitrate = size (bits) / duration (seconds) / 1000
     size = get_file_size(path) * 8
     duration = get_audio_info(path)['duration']
-    print("duration: ", duration)
     return (size / duration) / 1000
+
+# TODO fs and wb are hardcoded
+def calc_pesq(original_path, decoded_path):
+    original_vector = load_audio(original_path)[0]
+    decoded_vector = load_audio(decoded_path)[0]
+    return pesq(fs=16000, ref=original_vector, deg=decoded_vector, mode='wb')
 
 def get_file_size(path):
     return os.path.getsize(path)
@@ -17,7 +25,11 @@ def get_file_size(path):
 if __name__ == "__main__":
     import sys
 
-    if len(sys.argv) > 0:
-        sound_file = sys.argv[1]
+    if len(sys.argv) == 3:
+        original_path = sys.argv[1]
+        decoded_path = sys.argv[2]
+        print("PESQ score:", calc_pesq(original_path, decoded_path))
 
-        print("bitrate (Kbps) ", calc_bitrate(sound_file ))
+    elif len(sys.argv) == 2:
+        sound_file = sys.argv[1]
+        print("Bitrate (kbps):", calc_bitrate(sound_file))
